0659afbc959ef251eb9f030f89b5e0478c59b913
Monica Seaman <monica.l.seaman@gmail.com>
2018-09-07 20:14:12 +0000

    created controller actions for creating a new recommendation

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -8,4 +8,23 @@ class RecommendationsController < ApplicationController
     end
   end
 
+  get '/recommendations/new' do
+    if logged_in?
+      erb :'recommendations/create_recommendation'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/recommendations' do
+    @recommendation = Recommendation.create(params[:recommendation])
+    @recommendation.user_id = session[:user_id]
+    if @recommendation.save
+      redirect "/recommendations/#{@recommendation.id}"
+    else
+      #add flash message for the new recommendation failed to be created, please try again
+      redirect "/recommendations/new"
+    end
+  end
+
 end
