41bd18bfa021f20453d4ab7fb3499c778315ea09
Monica Seaman <monica.l.seaman@gmail.com>
2018-09-07 14:52:21 +0000

    CreateRecommendations migration

--- db/migrate/20180907144557_create_recommendations.rb
+++ db/migrate/20180907144557_create_recommendations.rb
@@ -1,4 +1,10 @@
 class CreateRecommendations < ActiveRecord::Migration
   def change
+    create_table :recommendations do |t|
+      t.string :title
+      t.string :description
+      t.integer :user_id
+      t.integer :category_id
+    end
   end
 end
