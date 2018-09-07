e27bed54f975486aa3772c3651d1d12823d75795
Monica Seaman <monica.l.seaman@gmail.com>
2018-09-07 15:23:53 +0000

    created ApplicationController

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    #set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  #could also be handled as separate handlers class (see sinatra-logging-in-and-out)
+  helpers do
+    def logged_in?
+      !!current_user
+      #!!session[:user_id]
+    end
+
+    def current_user
+      User.find_by(:id => session[:user_id])
+    end
+  end
+
+end
