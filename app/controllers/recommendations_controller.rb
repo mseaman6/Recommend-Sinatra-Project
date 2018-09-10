a6db559f0fc66a2485f444831d52f59758596d6f
Monica Seaman <monica.l.seaman@gmail.com>
2018-09-10 16:03:18 +0000

    added edit_recommendation view and controller actions and delete controller action

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -36,4 +36,43 @@ class RecommendationsController < ApplicationController
     end
   end
 
+  get '/recommendations/:id/edit' do
+    if logged_in?
+      #if @recommendation = current_user.recommendations.find_by(params[:id])
+      @recommendation = Recommendation.find(params[:id])
+      if current_user.id == @recommendation.user_id
+        erb :'recommendations/edit_recommendation'
+      else
+        redirect "/recommendations/#{params[:id]}"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/recommendations/:id' do
+    @recommendation = Recommendation.find(params[:id])
+    @recommendation.update(params[:recommendation])
+    if @recommendation.save
+      redirect "/recommendations/#{@recommendation.id}"
+    else
+      #add flash message for the edit recommendation failed, please try again
+      redirect "/recommendations/#{@recommendation.id}/edit"
+    end
+  end
+
+  delete '/recommendations/:id/delete' do
+    if logged_in?
+      @recommendation = Recommendation.find(params[:id])
+      if current_user.id == @recommendation.user_id
+        @recommendation = Recommendation.destroy(params[:id])
+        redirect '/recommendations'
+      else
+        redirect '/recommendations'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
 end
